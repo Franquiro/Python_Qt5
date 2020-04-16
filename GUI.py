9e9d6c71ef3db669828f786cb3ed4492a326aef5
Franquiro <franquiro@gmail.com>
2020-04-16 18:42:53 -0300

    Primer GUI

---
+++ GUI.py
@@ -0,0 +1,35 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import *
+
+def on_button_clicked():
+    alert = QMessageBox()
+    alert.setText('Cliqueaste el boton {}'.format())
+    alert.exec_()
+
+# Creo la aplicacion
+app = QApplication([])
+app.setStyle('Fusion')
+app.setStyleSheet("QWidget{background-color:#333333; color:#FFFFFF;}")
+# creo la ventana principal
+window = QWidget()
+# creo la etiqueta superior
+label = QLabel()
+label.setText('Hola')
+# Creo los botones
+btn_A = QPushButton('Boton A')
+btn_B = QPushButton('Boton B')
+# Creo un Layout
+layout = QVBoxLayout()
+# agrego los elementos al layout
+layout.addWidget(label)
+layout.addWidget(btn_A)
+layout.addWidget(btn_B)
+# conecto el boton A con el evento definido al principio
+btn_A.clicked.connect(on_button_clicked)
+# agrego el layout a la ventana
+window.setLayout(layout)
+#ejecuto la app
+window.show()
+app.exec_()
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Python_Qt5
+
+[Guia de PyQt5](https://www.zetcode.com/gui/pyqt5)
+* GUI.py - Primer intento de interfaz grafica en python con Qt
